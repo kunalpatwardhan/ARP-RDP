61b7238a7265a4eec50dcb6917bbb446c2506094
Kunal Patwardhan <kunal.patwardhan@gmail.com>
2021-01-12 21:25:14 +0530

    Add project files.

---
+++ WindowsFormsApp3/ARP-RDP.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 116);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(1, 122);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(213, 260);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 386);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ARP-RDP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp3/ARP-RDP.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] arp_output;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string split_ip;
+            split_ip = listBox1.Text.Split(' ')[1];
+            split_ip = split_ip.Split('.')[0] + '.' + split_ip.Split('.')[1] + '.' + split_ip.Split('.')[2];
+            listBox2.Items.Clear();
+            foreach (string str in arp_output)
+            {
+                if (str.Contains(split_ip) && (!str.Contains("Interface")))
+                {
+                    if(str.Contains("ff-ff-ff-ff-ff-ff"))
+                    {
+                        break;
+                    }
+                    string[] split_ip2 = str.Trim().Split(' ');
+                    listBox2.Items.Add(split_ip2[0]);
+                }
+            }
+            //listBox2.Items.Add(split_ip);
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!listBox2.Text.Contains("Port closed"))
+                Process.Start("mstsc", "/v:" + listBox2.Text);
+
+
+                  
+            
+
+        }
+        string output;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            // Start the child process.
+            Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "arp";
+            p.StartInfo.Arguments = "-a";
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output2 = p.StandardOutput.ReadToEnd();
+            if (output == output2)
+            {
+                using (TcpClient tcpClient = new TcpClient())
+                {
+
+                    for (int i = 0; i < listBox2.Items.Count; i++)
+                    {
+                        try
+                        {
+                            if (!listBox2.Items[i].ToString().Contains("Port closed"))
+                                tcpClient.Connect(listBox2.Items[i].ToString(), 3389);
+                            //Process.Start("mstsc", "/v:" + listBox2.Text);
+                        }
+                        catch (Exception)
+                        {
+                            listBox2.Items[listBox2.Items.IndexOf(listBox2.Items[i].ToString())] = listBox2.Items[i].ToString() + " - Port closed";
+                        }
+                    }
+                }
+                return;
+            }
+            output = output2;
+            arp_output = output.Split(new[] { Environment.NewLine },
+    StringSplitOptions.None);
+            foreach (string str in arp_output)
+            {
+                if (str.Contains("Interface"))
+                    listBox1.Items.Add(str);
+            }
+
+            p.WaitForExit();
+            
+        }
+    }
+}
